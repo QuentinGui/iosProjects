bf64314ed426a407339b3f288a45802db8ab99cc
quentingui <quentingui@foxmail.com>
2014-08-05 18:59:02 +0800

    Initial Commit

---
+++ QingMicro/QingMicro/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  QingMicro
+//
+//  Created by guiqing on 8/5/14.
+//  Copyright (c) 2014 Personal. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
