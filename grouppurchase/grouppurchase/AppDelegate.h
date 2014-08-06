ddccdef2c7a1110ef1e03dc128c8d7b99cea47ba
quentingui <quentingui@foxmail.com>
2014-08-06 11:28:29 +0800

    init project

---
+++ grouppurchase/grouppurchase/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  grouppurchase
+//
+//  Created by guiqing on 8/6/14.
+//  Copyright (c) 2014 Personal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
